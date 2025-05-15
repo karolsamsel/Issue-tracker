75859a4220b75b17c68d44f7b4033d0b991094f9
karolsamsel <karol.samsel.dev@gmail.com>
2025-05-15 21:04:06 +0200

    refactored Issues page

---
+++ app/issues/list/IssuesTable.tsx
@@ -0,0 +1,78 @@
+import { IssueStatusBadge } from "@/app/components";
+import { Issue, Status } from "@prisma/client";
+import { Table } from "@radix-ui/themes";
+import { default as Link, default as NextLink } from "next/link";
+import { columns } from "./page";
+
+interface Props {
+  searchParams: {
+    status: Status;
+    orderBy: keyof Issue;
+    page: string;
+  };
+  issues: Issue[];
+}
+
+const IssuesTable = ({ issues, searchParams }: Props) => {
+  return (
+    <Table.Root variant="surface" mb={"4"}>
+      <Table.Header>
+        <Table.Row>
+          {columns.map((column) => (
+            <Table.ColumnHeaderCell
+              key={column.value}
+              className={column.className}
+            >
+              <NextLink
+                href={{
+                  query: { ...searchParams, orderBy: column.value },
+                }}
+              >
+                {column.label}
+              </NextLink>
+              {column.value === searchParams.orderBy && (
+                <svg
+                  width="15"
+                  height="15"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="inline"
+                >
+                  <path
+                    d="M7.14645 2.14645C7.34171 1.95118 7.65829 1.95118 7.85355 2.14645L11.8536 6.14645C12.0488 6.34171 12.0488 6.65829 11.8536 6.85355C11.6583 7.04882 11.3417 7.04882 11.1464 6.85355L8 3.70711L8 12.5C8 12.7761 7.77614 13 7.5 13C7.22386 13 7 12.7761 7 12.5L7 3.70711L3.85355 6.85355C3.65829 7.04882 3.34171 7.04882 3.14645 6.85355C2.95118 6.65829 2.95118 6.34171 3.14645 6.14645L7.14645 2.14645Z"
+                    fill="currentColor"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              )}
+            </Table.ColumnHeaderCell>
+          ))}
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {issues.map((issue) => {
+          return (
+            <Table.Row key={issue.id}>
+              <Table.RowHeaderCell>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                <div className="md:hidden">
+                  Status <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.RowHeaderCell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          );
+        })}
+      </Table.Body>
+    </Table.Root>
+  );
+};
+
+export default IssuesTable;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Pagination from "./components/Pagination";
 
 export default function Home() {
-  return (
-    <div>
-      <Pagination currentPage={2} itemCount={100} pageSize={10} />
-    </div>
-  );
+  return <div>Home page</div>;
 }
